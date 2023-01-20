84962c3336476c318eabf1a32883f31b22dac7af
MObinXIV <mobinxiv@gmail.com>
2023-01-20 18:46:27 +0200

    save changes

---
+++ TheoryPart/OO/The5solidPrinciples/SRP/example/logger.js
@@ -0,0 +1,30 @@
+import logMessage from "./logger"
+
+class CalorieTracker{
+    constructor(maxCalories){
+        this.maxCalories=maxCalories
+        this.currentCalories=0
+    }
+
+
+    trackCalories(caloriesCount)
+    {
+
+        this.currentCalories+=caloriesCount
+
+        if(this.currentCalories > this.maxCalories)
+
+            logMessage('Max calories exceeded')
+        
+    }
+
+
+            // This function violets the SRP , so we will move it to a module 
+    /*logCaloriesPlus(){
+        console.log('Max calories exceeded')
+    }*/
+}
+
+ // After we change the calorie tracker has only reason to change is how we tracking our calories 
+
+ // Also logger has only to notify the user with any way we want 
